0b5422a1355abea63fd1f76b0d3becc8ed198313
shao0213 <B10823055@gemail.yuntech.edu.tw>
2023-07-28 21:53:19 +0800

    還在試dropzone js
    抓不到傳過來的檔案 回傳none

--- ERP/urls.py
+++ ERP/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Frontend.urls")),
     path('restful/', include("Backend.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
